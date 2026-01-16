c437f904a687d1b2670fabe87c038f65066c0f12
Roop Teja <roopteja52@gmail.com>
2026-01-16 18:14:29 +0530

    Added Coverage

---
+++ Authentication/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    collectCoverageFrom: ['utils/**/*.js'],
+    coverageThreshold: {
+        global: {
+            statements: 60,
+            branches: 50,
+            functions: 60,
+            lines: 60,
+        },
+    },
+};
