12f61cd6ca1c263ceeb6b492f92953daeeef10b6
Roop Teja <roopteja52@gmail.com>
2026-01-16 23:09:03 +0530

    Fix: Format Fixed

--- Feed/Model/post.js
+++ Feed/Model/post.js
@@ -12,7 +12,7 @@ const PostSchema = new mongoose.Schema(
         },
         mediaURL: {
             type: String,
-            default: "",
+            default: '',
         },
         title: {
             type: String,
